19e5b99c610bb64eaed26b78e7c686ae8ac6103c
Tanmay Virkar <tanmay.virkar@bistasolutions.com>
2016-11-10 19:11:00 +0530

    Added Pivot Task, Logger task

--- extra_tasks/models/commission_report.py
+++ extra_tasks/models/commission_report.py
@@ -1,50 +1,71 @@
 from openerp import fields,models,api, _
 from openerp.exceptions import except_orm
+from openerp import tools
 
 class ResUsers(models.Model):
-    _inherit = ['res.users']
+    _name ='res.users'
+    _inherit = ['res.users','mail.thread']
 
     def calculate_commission(self):
         print True
 
     commission = fields.Float("Commission",default='1.0')
 
-    # @api.onchange('commission')
-    # def on_commission_change(self):
-        # if self.commission > 5:
-        #     raise except_orm("Warning", "Commission cannot be more than 5%")
-        # elif self.commission < 1:
-        #     raise except_orm("Warning", "Commission cannot be less than 1%")
-        # if self.commission < 1:
-        #     raise except_orm("Warning", "Commission cannot be less than 1%")
+    # @api.model
+    # def create(self,vals):
+    #     res = super(ResUsers,self).create(vals)
+    #     print "Create Commission Context",self._context
+    #     print "Create Commission Vals",vals
+    #
+    #     if vals['commission'] > 5.0:
+    #         # curr_commission = vals['commission']
+    #         # if curr_commission > 5.0:
+    #         raise except_orm("Warning", "Commission cannot be more than 5%")
+    #     elif vals['commission'] < 1.0:
+    #         raise except_orm("Warning", "Commission cannot be less than 1%")
+    #     return res
 
-    @api.model
-    def create(self,vals):
-        res = super(ResUsers,self).create(vals)
-        print "Create Commission Context",self._context
-        print "Create Commission Vals",vals
+    # @api.multi
+    # def write(self,vals):
+    #     user_id = self._context.get('params')['id']
+    #     if vals['commission'] > 5.0:
+    #         raise except_orm("Warning", "Commission cannot be more than 5%")
+    #     elif vals['commission'] < 1.0:
+    #         raise except_orm("Warning", "Commission cannot be less than 1%")
+    #     my_id = self.env['res.users'].browse(user_id)
+    #     print "my_id",my_id
+    #     log_message = _('Commission changed for %s i.e %s ->> %s' % (self.name, self.commission, vals['commission']))
+    #     get_message_post_id = my_id.message_post(body=log_message, type='comment', author_id=my_id.partner_id.id, attachments=[])
+    #     # print "get_message_post_id", get_message_post_id
+    #     if get_message_post_id:
+    #         get_message_post_id.write({'res_id': user_id, 'model': 'res.users'})
+    #     res = super(ResUsers, self).write(vals)
+    #     return res
 
-        if vals['commission'] > 0:
-            curr_commission = vals['commission']
-            if curr_commission > 5:
-                raise except_orm("Warning", "Commission cannot be more than 5%")
-            elif curr_commission < 1:
-                raise except_orm("Warning", "Commission cannot be less than 1%")
-        return res
-
+    # Ojas Ka Code
     @api.multi
-    def write(self,vals):
-        print"vals['commission']****",vals['commission'], type(vals['commission'])
-        print "Write Commission Context", self._context
-        print "Write Commission Vals", vals
-        if vals['commission'] > 5.0:
-            # curr_commission = vals['commission']
-            # if curr_commission > 5.0:
-            raise except_orm("Warning", "Commission cannot be more than 5%")
-        elif vals['commission'] < 1.0:
-            raise except_orm("Warning", "Commission cannot be less than 1%")
-        res = super(ResUsers, self).write(vals)
-        return res
+    def write(self, vals):
+        # print "FIELDS WRITE=============================="
+        var = vals.get('commission', 0)
+        # print "FIELDS WRITE=====================", var
+        if var >= 1 and var <= 5:
+            pass
+        else:
+            raise except_orm(_("Values Should be Between 1 to 5 Only!!!"))
+        # print "SELF===============================", self.commission_field
+        # print "SELF===============================", self.name
+        # print "VALS===============================", vals['commission_field']
+        # print "CONTXT========================", self._context.get('params')['id']
+        my_id = self._context.get('params')['id']
+        record_id = self.env['res.users'].browse(my_id)
+        # print "record", record_id
+        log_message = _('Options changed for %s i.e %s ->> %s' % (self.name, self.commission, vals['commission']))
+        a = record_id.message_post(body=log_message, type='comment', author_id=record_id.partner_id.id, attachments=[])
+        print "a", a
+        if a:
+            a.write({'res_id': my_id, 'model': 'res.users'})
+        return super(ResUsers, self).write(vals)
+    # Ojas Ka Code
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -62,8 +83,8 @@ class SaleOrder(models.Model):
         vals['commission'] = get_user_commission.commission
         res = super(SaleOrder, self).create(vals)
         return res
-
+#
 # Not Working Already Tried
-class MailThread(models.Model):
-    _name ='res.users'
-    _inherit = ['mail.thread']
+# class MailThread(models.Model):
+#     _inherit = ['mail.thread']
+
--- extra_tasks/reports/report_function.py
+++ extra_tasks/reports/report_function.py
@@ -1,8 +1,6 @@
 from openerp.osv import osv
 from openerp.report import report_sxw
-from datetime import datetime
 
-from pytz import timezone
 
 
 class CommissionReport(report_sxw.rml_parse):
@@ -13,70 +11,36 @@ class CommissionReport(report_sxw.rml_parse):
                     'get_commission':self.get_commission,
             })
 
-    # def get_commission(self):
-    #     # print "get_commission self._context in report_function.py",self.localcontext
-    #     # get_partner_id = self.pool.get('res.users').browse(self.cr, self.uid, self.localcontext['active_ids'])
-    #     # print get_partner_id
-    #     get_user_details = self.pool.get('res.users').browse(self.cr, self.uid, self.localcontext['active_ids'])
-    #     print get_user_details
-    #     user_name = get_user_details.partner_id.name
-    #     print user_name
-    #     # Save partner ID in variable
-    #     # partner_id = get_partner_id.partner_id.id
-    #     # get_user_id =
-    #     # print partner_id
-    #     get_all_saleorders = self.pool.get('sale.order').search(self.cr, self.uid,[('user_id', '=', self.localcontext['active_ids']),('state','=','done')])
-    #     total_commission = 0
-    #     sale_order_total_amount_sum = 0
-    #     single_sale_order_row = ''
-    #     for each_sale_order in get_all_saleorders:
-    #         get_sale_order_details = self.pool.get('sale.order').browse(self.cr, self.uid, each_sale_order)
-    #         print get_sale_order_details.name
-    #         sale_order_name = get_sale_order_details.name
-    #         print "Amount Total", get_sale_order_details.amount_total
-    #         sale_order_total_amount = get_sale_order_details.amount_total
-    #         sale_order_total_amount_sum = sale_order_total_amount_sum + sale_order_total_amount
-    #         print "Commission Value",  get_user_details.commission
-    #         commission = (get_sale_order_details.amount_total * get_user_details.commission)/100
-    #         print "Commission",commission
-    #         curr_commission = commission
-    #         total_commission = total_commission + commission
-    #         single_sale_order_row = single_sale_order_row + "<tr><td>"+user_name+"</td><td>"+sale_order_name+"</td><td>" + str(sale_order_total_amount) + "</td><td>" + str(curr_commission) + "</td></tr>"
-    #     return single_sale_order_row + "<tr><td colspan='2'><b>Total Sale Order Amount</b></td><td colspan='2'>"+str(sale_order_total_amount_sum)+"</td></tr><tr><td colspan='2'><b>Total Commission</b></td><td colspan='2'>"+str(total_commission)+"</td></tr>"
-
     def get_commission(self):
         get_user_details = self.pool.get('res.users').browse(self.cr, self.uid, self.localcontext['active_ids'])
-        print get_user_details
+        # Current User for whom the report is being printed
         user_name = get_user_details.partner_id.name
-        print user_name
+        # Get all sale order related to user above
         get_all_saleorders = self.pool.get('sale.order').search(self.cr, self.uid,
                                                                 [('user_id', '=', self.localcontext['active_ids']),
                                                                  ('state', '=', 'done')])
-        total_commission = 0
-        sale_order_total_amount_sum = 0
-        single_sale_order_row = ''
+        # Initialize an empty list
+        single_sale_order_row = []
+        # Loop through each sale order result set i.e. set returned by search query
         for each_sale_order in get_all_saleorders:
+            # Get Single Sale Order Details
             get_sale_order_details = self.pool.get('sale.order').browse(self.cr, self.uid, each_sale_order)
-            print get_sale_order_details.name
+            # Sale Order Name E.g. SO010
             sale_order_name = get_sale_order_details.name
-            print "Amount Total", get_sale_order_details.amount_total
+            # Total Sale Order Amount
             sale_order_total_amount = get_sale_order_details.amount_total
-            sale_order_total_amount_sum = sale_order_total_amount_sum + sale_order_total_amount
-            print "Commission Value", get_sale_order_details.commission
+            # Percentage Commission per Sale Order
             commission_per_sale_order = get_sale_order_details.commission
-            commission = (get_sale_order_details.amount_total * commission_per_sale_order) / 100
-            print "Commission", commission
-            curr_commission = commission
-            total_commission = total_commission + commission
-            single_sale_order_row = single_sale_order_row + "<tr><td>" + user_name + "</td><td>" + sale_order_name + "</td><td>" + str(
-                sale_order_total_amount) + "</td><td>" + str(curr_commission) + "</td></tr>"
-        return single_sale_order_row + "<tr><td colspan='2'><b>Total Sale Order Amount</b></td><td colspan='2'>" + str(
-            sale_order_total_amount_sum) + "</td></tr><tr><td colspan='2'><b>Total Commission</b></td><td colspan='2'>" + str(
-            total_commission) + "</td></tr>"
+            # Commission Amount per Sale Order
+            curr_commission_amount = (get_sale_order_details.amount_total * commission_per_sale_order) / 100
+            # Append each value to a list in order to form a single row of values
+            single_sale_order_row.append({'Username':user_name,'SaleOrder':sale_order_name,'SaleOrderAmount':sale_order_total_amount,'CurrCommission':curr_commission_amount})
+        # Return the list
+        return single_sale_order_row
 
 
 class ReportManagementQweb(osv.AbstractModel):
-    #    name must be report.module_name.template_id
+    # name must be report.module_name.template_id
     _name ='report.extra_tasks.report_res_users_commission'
     _inherit = 'report.abstract_report'
     _template = 'extra_tasks.report_res_users_commission'
